b35eb6b41311f993c859298a0ddbe66f9f49c8e2
Aadhar Chandiwala <chandiwalaaadhar@gmail.com>
2021-07-25 18:25:35 +0530

    Developement Setup

---
+++ src/components/Intro.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+import {
+  img_container,
+  title,
+  sub_container,
+  coming_soon,
+} from "./intro.module.scss";
+import DevelopementImage from "../assets/dev_img.png";
+import WebFont from "webfontloader";
+
+const Intro = () => {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Poppins", "Dancing Script"],
+      },
+    });
+  }, []);
+  return (
+    <div>
+      <div className={sub_container}>
+        <h2 className={title}>Aadhar Chandiwala</h2>
+      </div>
+
+      <img
+        className={img_container}
+        src={DevelopementImage}
+        alt="Website Under Developement"
+      />
+      <div className={sub_container}>
+        <h2 className={coming_soon}>Coming Soon...</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
